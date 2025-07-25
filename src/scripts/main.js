c411f411b95f4e3c5405cf8e758cb1ab7e24e8dd
Denis <denchick1546450@gmail.com>
2025-07-25 20:09:28 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-// write code here
+const tree = document.querySelector('.tree');
+const listItems = tree.querySelectorAll('li');
+
+// Оборачиваем текст каждого <li> в <span>
+listItems.forEach(li => {
+  const text = li.firstChild.textContent.trim();
+  const span = document.createElement('span');
+  span.textContent = text;
+
+  li.firstChild.replaceWith(span);
+});
+
+// Вешаем обработчик
+tree.addEventListener('click', e => {
+  if (e.target.tagName !== 'SPAN') return;
+
+  const li = e.target.closest('li');
+  const ul = li.querySelector('ul');
+
+  if (ul) {
+    ul.hidden = !ul.hidden;
+  }
+});
